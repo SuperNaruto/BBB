8561bb33cd0d7d8a551781b2849f0baf2fe832be
small-seven <353193036@qq.com>
2016-03-29 10:59:50 +0800

    55

--- app/src/main/java/com/example/vdllo/bbb/MainActivity.java
+++ app/src/main/java/com/example/vdllo/bbb/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     //完成注册
     String m;
+    String s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         m = "";
+        s = "小胡我饿了";
     }
 }
