25d873e55254e7ee10fb68fb3fb71e229e41753b
刘博 <317154369@qq.com>
2016-03-29 10:55:55 +0800

    完成登录

--- app/src/main/java/com/example/vdllo/bbb/MainActivity.java
+++ app/src/main/java/com/example/vdllo/bbb/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 刘博登录
+    String m;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m += "asdads";
+
     }
 }
